2e3adfda5564095e6a8b406d1a4114289c14562b
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-21 17:36:31 +0000

    Fixed routing, added navbar redirecting

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'spots#index', as: 'home'
-
   resources :spots
+
+  authenticated :user do
+     root 'spots#index', as: :authenticated_root
+   end
+
+   unauthenticated do
+     root 'home#index', as: :unauthenticated_root
+   end
 
 end
