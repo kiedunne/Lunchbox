725948389f430a9c044f114cf9dc2961dd000a7c
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-21 13:45:01 +0000

    Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+  root 'home#after_login', as: :authenticated_root
+end
+
+unauthenticated do
+  root "home#index", as: :unauthenticated_root
+end
+
 end
