cfa3a82700da7557f9eea04cfa90f89585f307ba
kiedunne <kie.dunne@gmail.com>
2019-01-21 17:45:06 +0000

    added profile path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'spots#index', as: 'home'
-
+  get 'users/:id', to: 'users#show', as: :profile
   resources :spots
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_21_135856) do
     t.text "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
-
 
   create_table "spots", force: :cascade do |t|
     t.string "spot"
@@ -34,6 +32,5 @@ ActiveRecord::Schema.define(version: 2019_01_21_135856) do
     t.text "info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 end
