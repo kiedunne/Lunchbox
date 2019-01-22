5e1fe20e9ec811e905daab049d5d3d8ea8d8e538
Asad Khan <asad_khan47@hotmail.co.uk>
2019-01-22 12:45:00 +0000

    Added migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_112856) do
+ActiveRecord::Schema.define(version: 2019_01_22_124236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_01_22_112856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
