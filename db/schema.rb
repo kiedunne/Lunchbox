05a9a2d552d10ed3152802568efc59ed4436df50
Asad Khan <asad_khan47@hotmail.co.uk>
2019-01-22 12:06:40 +0000

    Attempt 8 at travis, ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_110102) do
-
+ActiveRecord::Schema.define(version: 2019_01_22_112856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +43,6 @@ ActiveRecord::Schema.define(version: 2019_01_22_110102) do
     t.text "info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.string "username"
   end
 
@@ -56,11 +54,11 @@ ActiveRecord::Schema.define(version: 2019_01_22_110102) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "username", null: false
+    t.text "username"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-end 
+end
