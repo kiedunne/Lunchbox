aac3066042c7d2788a7eaf471a212d27a50ca154
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-22 11:09:48 +0000

    User can have avatar

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,41 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2019_01_21_135856) do
+
+ActiveRecord::Schema.define(version: 2019_01_22_110102) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "spots", force: :cascade do |t|
+    t.string "spot"
+    t.text "time"
+    t.text "location"
+    t.text "info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,20 +53,11 @@ ActiveRecord::Schema.define(version: 2019_01_21_135856) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "username", null: false
+    t.text "username"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
-
-  create_table "spots", force: :cascade do |t|
-    t.string "spot"
-    t.text "time"
-    t.text "location"
-    t.text "info"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
-  end
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
