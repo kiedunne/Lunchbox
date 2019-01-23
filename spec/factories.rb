df2e76a7a545dbbf93e3bca39aa48ade21fd26b0
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-23 11:21:31 +0000

    Solving bug with test

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,6 +7,12 @@ FactoryBot.define do
     username { "John" }
   end
 
+  factory :spot do
+    spot { "hello, world!" }
+    location { "hello, world!" }
+    info { "hello, world!" }
+  end
+
   sequence :email do |n|
     "email#{n}@gmail.com"
   end
