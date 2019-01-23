b1bd5fdacc9dcc7aa6cb7ba2fc151ab6ae8dc76b
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-23 21:52:40 +0000

    More test to spot controller

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -24,9 +24,9 @@ RSpec.describe SpotsController, type: :controller do
 				end
 
 				describe "Get #show" do
-					 it 'should show post' do
-						  post = create_spot
-						  get :show, params: { id: post.id }
+					 it 'should show spot' do
+						  spot = create_spot
+						  get :show, params: { id: spot.id }
 						  expect(response.status).to eq(200)
 					 end
 				end
@@ -36,11 +36,33 @@ RSpec.describe SpotsController, type: :controller do
 					 		expect { create_spot }.to change { Spot.count }.by(1)
 					 end
 				end
-		end
-		describe "GET #new " do
-			 it "responds with 302" do
-				  get :new
-				  expect(response).to have_http_status(302)
-			 end
+		
+			describe "GET #new " do
+				 it "responds with 200" do
+					  get :new
+					  expect(response).to have_http_status(200)
+				 end
+			end
+
+			describe "DELETE #destroy" do
+				 it "deletes a spot from db" do
+					  spot = create_spot
+					  expect { delete_post(spot.id) }.to change { Spot.count }.by(-1)
+				 end
+
+				 it "responds with a redirect" do
+				 	 spot = create_spot
+				 	 delete_post(spot.id)
+				 	 expect(response).to have_http_status(302)
+				 end
+			end
+
+			describe "POST #update" do
+				 it "Update a post" do
+					  spot = create_spot
+					  update_spot(spot.id)
+					  expect(response).to redirect_to(spot)
+				 end
+			end
 		end
 end
--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -2,8 +2,21 @@ def create_spot
   Spot.create!(params)
 end
 
-def delete_post
-  delete :destroy, params: { id: post_in_db.id }
+def delete_post(spot_id)
+  delete :destroy, params: { :id => spot_id }
+end
+
+def update_spot(spot_id)
+	 put :update, :params => { :id => spot_id, :spot => {
+	 	spot: 'test2', 
+		time: 'now2', 
+		location: 'close2', 
+		info: 'test2',
+		user_id: user.id, 
+		username: user.username, 
+		time_start: '7', 
+		time_end: '8'
+	  } }
 end
 
 def params
