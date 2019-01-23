55611fa9f98aec5357f8763729070ef159c29960
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-23 20:54:35 +0000

    adding more test to the controller

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -25,23 +25,22 @@ RSpec.describe SpotsController, type: :controller do
 
 				describe "Get #show" do
 					 it 'should show post' do
-						 post = Spot.create!(spot: 'test', time: 'now', location: 'close', info: 'test',
-						 	user_id: user.id, username: user.username, time_start: '6', time_end: '7' 
-						 	)
-						 get :show, params: { id: post.id }
-						 expect(response.status).to eq(200)
-					end
+						  post = create_spot
+						  get :show, params: { id: post.id }
+						  expect(response.status).to eq(200)
+					 end
 				end
-				# describe "GET #show" do
-				# 		it "responds with 200" do
-				# 				get :show, params: { id: test_spot.id }
-				# 				expect(response).to have_http_status(200)
-				# 		end
-				# end
-				# describe "SPOST #create" do
-				# 	 it "responds creates a spot in db" do
-				# 	 		expect { create_spot }.to change { Spot.count }.by(1)
-				# 	 end
-				# end
+
+				describe "SPOST #create" do
+					 it "responds creates a spot in db" do
+					 		expect { create_spot }.to change { Spot.count }.by(1)
+					 end
+				end
+		end
+		describe "GET #new " do
+			 it "responds with 302" do
+				  get :new
+				  expect(response).to have_http_status(302)
+			 end
 		end
 end
--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,10 +1,20 @@
 def create_spot
-  spot = FactoryBot.create(:spot)
-  user_with_spot = FactoryBot.create(:user, spot: spot)
-  allow(User).to receive(:find).and_return(user_with_spot)
-  spots :create, params: { spot: { id: test_post.id, spot: test_post.spot, location: spot.location, username: spot.user.username } }
+  Spot.create!(params)
 end
 
 def delete_post
   delete :destroy, params: { id: post_in_db.id }
+end
+
+def params
+	 {
+	spot: 'test', 
+	time: 'now', 
+	location: 'close', 
+	info: 'test',
+	user_id: user.id, 
+	username: user.username, 
+	time_start: '6', 
+	time_end: '7'
+	}
 end
