3bb1b51202121e646165dc0f86de41c2e11a9530
kiedunne <kie.dunne@gmail.com>
2019-01-24 12:49:51 +0000

    follow button logic

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,8 @@
 class Spot < ApplicationRecord
   validates :spot, presence: true, length: { minimum: 3 }
+  validates :time_start, presence: true, length: { minimum: 5 }
+  validates :time_end, presence: true, length: { minimum: 5 }
+  validates :spot, presence: true, length: { minimum: 3 }
   acts_as_followable
   belongs_to :user
   geocoded_by :location
