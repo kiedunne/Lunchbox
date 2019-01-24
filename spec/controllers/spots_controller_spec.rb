1d4bad2ee91c506008a40f978b14c664edf7b900
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-24 13:39:44 +0000

    Test finished to spot and user controller

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -3,6 +3,7 @@ require_relative '../helper_spec.rb'
 
 RSpec.describe SpotsController, type: :controller do
 		let(:user) { FactoryBot.create(:user) }
+		let(:test_spot) { create_spot }
 		context "When user NOT Logged IN" do
 			 describe "GET #new " do
 			 	 it "responds with 302" do
@@ -10,9 +11,14 @@ RSpec.describe SpotsController, type: :controller do
 					   expect(response).to have_http_status(302)
 				  end
 			 end
+			 describe "POST #create" do
+			 	 it "Response with 302" do
+			 		  post :create
+			 		  expect(response).to redirect_to("/users/sign_in")
+			 	 end
+			 end
 		end
 		context "When Logged IN" do
-				let(:test_spot) { FactoryBot.create(:spot) }
 				before :each do
 						sign_in(user, scope: :user)
 				end
@@ -25,8 +31,7 @@ RSpec.describe SpotsController, type: :controller do
 
 				describe "Get #show" do
 					 it 'should show spot' do
-						  spot = create_spot
-						  get :show, params: { id: spot.id }
+						  get :show, params: { id: test_spot.id }
 						  expect(response.status).to eq(200)
 					 end
 				end
@@ -38,39 +43,54 @@ RSpec.describe SpotsController, type: :controller do
 				end
 
 				describe "Spots #create" do
-					 it "Render to the post after creating it with 200" do
-					 	 spot = create_spot
-					 	 expect(response).to have_http_status(200)
+					 it "Render to the spot after creating it with 302" do
+					 	 post :create, params: {
+					 	 	spot: { 
+					 	 		id: test_spot.id,
+					 	 		spot: test_spot.spot,
+					 	 		time: test_spot.time,
+					 	 		location: test_spot.location, 
+								info: test_spot.info,
+								user_id: user.id, 
+								username: user.username, 
+								time_start: test_spot.time_start, 
+								time_end: test_spot.time_end
+					 	 		 } }
+					 	 expect(response).to have_http_status(302)
 					 end
 				end
-		
-			describe "GET #new " do
-				 it "responds with 200" do
-					  get :new
-					  expect(response).to have_http_status(200)
-				 end
-			end
+				describe "GET #new " do
+					 it "responds with 200" do
+						  get :new
+						  expect(response).to have_http_status(200)
+					 end
+				end
 
-			describe "DELETE #destroy" do
-				 it "deletes a spot from db" do
-					  spot = create_spot
-					  expect { delete_post(spot.id) }.to change { Spot.count }.by(-1)
-				 end
+				describe "DELETE #destroy" do
+					 it "deletes a spot from db" do
+						  spot = create_spot
+						  expect { delete_post(spot.id) }.to change { Spot.count }.by(-1)
+					 end
 
-				 it "responds with a redirect" do
-				 	 spot = create_spot
-				 	 delete_post(spot.id)
-				 	 expect(response).to have_http_status(302)
-				 end
-			end
+					 it "responds with a redirect" do
+					 	 delete_post(test_spot.id)
+					 	 expect(response).to have_http_status(302)
+					 end
+				end
 
-			describe "POST #update" do
-				 it "Update a post" do
-					  spot = create_spot
-					  update_spot(spot.id)
-					  expect(response).to redirect_to(spot)
-				 end
-			end
+				describe "POST #update" do
+					 it "Update a spot" do
+						  update_spot(test_spot.id)
+						  expect(response).to redirect_to(test_spot)
+					 end
+				end
+
+				describe "GET #find" do
+					 it "find a spot" do
+						  get :edit, params: { id: test_spot.id }
+						  expect(response.status).to eq(200)
+					 end
+				end
 		end
 		after :each do
 			 sign_out(user)
