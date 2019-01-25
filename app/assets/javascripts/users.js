de6fa5f8f1385a197b6eda8071919fb69a959777
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-25 13:26:27 +0000

    Added Js for comments

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,6 @@
 function showDiv() {
    document.getElementById('UserPosts').style.display = "block";
 }
+function showComments() {
+   document.getElementById('UserComments').style.display = "block";
+}
