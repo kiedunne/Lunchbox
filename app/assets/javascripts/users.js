83645ebd7afd3b1574be1883ca1a42bf8e6a8771
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-25 13:35:53 +0000

    Edited My Spots show and hide

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,12 @@
 function showDiv() {
-   document.getElementById('UserPosts').style.display = "block";
+   var x = document.getElementById("UserPosts");
+ if (x.style.display === "none") {
+   x.style.display = "block";
+ } else {
+   x.style.display = "none";
+  }
 }
+
 function showComments() {
   var x = document.getElementById("UserComments");
 if (x.style.display === "none") {
