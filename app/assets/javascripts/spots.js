b759729aeaa03a5b42e0b8a4f0b52c3cb61e0126
kiedunne <kie.dunne@gmail.com>
2019-01-26 17:02:36 +0000

    refactored users partials and spot style

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -1,10 +1,19 @@
-$( function() {
-   $( "#datepicker" ).datepicker();
- } );
 
-var date = new Date();
-var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+$(document).ready(ready);
+$(document).on('page:load', ready);
 
-$('#datepicker').datepicker({
-    minDate: today
-});
+var ready;
+ready = function() {
+
+  $( function() {
+     $( "#datepicker" ).datepicker();
+   } );
+
+  var date = new Date();
+  var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+  $('#datepicker').datepicker({
+      minDate: today
+  });
+
+};
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( users.js )
 Rails.application.config.assets.precompile += %w( spots.js )
 Rails.application.config.assets.precompile += %w( users.css )
 Rails.application.config.assets.precompile += %w( spots.css )
+Rails.application.config.assets.precompile += %w( comments.css )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
