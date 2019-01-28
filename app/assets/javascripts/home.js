58ad91d46ab6dcf99341fbf6c91c5399fcc3f08a
kiedunne <kie.dunne@gmail.com>
2019-01-28 15:37:54 +0000

    pick diet pref at signup

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,10 +7,33 @@ function closeForm() {
 }
 
 // The class you click to trigger the popup
-
 $('#signup-button').on('click', function() {
   // The Overlay fades in
   $("#formGroup").fadeIn(200, function() {});
   // The Popup fades in just after
-  // $(".popup").fadeIn(600, function() {});
+  $(".bkg-animated").fadeIn(600, function() {});
 });
+
+function veggieAdd() {
+  var Diet = document.getElementById("diet-pref").value = "vegetarian";
+}
+
+function pesciAdd() {
+  var Diet = document.getElementById("diet-pref").value = "pescetarian";
+}
+
+function veganAdd() {
+  var Diet = document.getElementById("diet-pref").value = "vegan";
+}
+
+function lactoAdd() {
+  var Diet = document.getElementById("diet-pref").value = "lacto-vegetarian";
+}
+
+function ovoAdd() {
+  var Diet = document.getElementById("diet-pref").value = "ovo-vegetarian";
+}
+
+function omniAdd() {
+  var Diet = document.getElementById("diet-pref").value = "standard";
+}
