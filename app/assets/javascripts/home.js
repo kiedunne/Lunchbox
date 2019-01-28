0be0f9a6fc69c12711f405afb71278ef8bc58182
kiedunne <kie.dunne@gmail.com>
2019-01-28 19:00:41 +0000

    login signup form formatted

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,32 +1,18 @@
-// function openForm() {
-//   document.getElementById("formGroup").style.display = "block";
-// }
-//
-// function closeForm() {
-//   document.getElementById("formGroup").style.display = "none";
-// }
-//
-// // The class you click to trigger the popup
-// $('#signup-button').on('click', function() {
-//   // The Overlay fades in
-//   $("#formGroup").fadeIn(200, function() {});
-//   });
-
-// $('#close-button').on('click', function() {
-//   // The Popup fades in just after
-//   $("#formGroup").fadeOut(600, function() {});
-//   });
 
   $(document).ready(function(){
-    $("#close-button").click(function(){
-      $("#formGroup").fadeOut();
-    });
     $("#signup-button").click(function(){
       $("#formGroup").fadeIn();
     });
+    $("#close-button").click(function(){
+      $("#formGroup").fadeOut();
+    });
+    $("#signin-button").click(function(){
+      $("#formGroupSignIn").fadeIn();
+    });
+    $("#close-signin-button").click(function(){
+      $("#formGroupSignIn").fadeOut();
+    });
   });
-
-
 
 function veggieAdd() {
   var Diet = document.getElementById("diet-pref").value = "vegetarian";
