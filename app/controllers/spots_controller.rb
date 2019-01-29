a9b1463567869b95196187c5c78ee7af0498ff0d
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-29 18:42:34 +0000

    Marks done

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -8,6 +8,10 @@ class SpotsController < ApplicationController
     @users = User.all
   end
 
+  def mateusz
+    @spots = Spot.all
+  end
+
   def show
     @spots = Spot.all
     @spot = Spot.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :spots
+
+get 'mateusz', to: 'spots#mateusz'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :spots do
