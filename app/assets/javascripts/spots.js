5f3f126ae728010ad3daf1867670baccbd1fc68d
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-30 12:38:47 +0000

    Working funcionality

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -24,8 +24,10 @@ function DisplayMap() {
   if (x.style.display === "none") {
   y.style.display = "none";
   x.style.display = "block";
+  $('#mapsbutton').text("Show Full Map");
 } else {
   x.style.display = "none";
   y.style.display = "block";
+  $('#mapsbutton').text("Back to spots");
 }
 }
