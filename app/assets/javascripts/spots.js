69eac82ee546e935ed7ddadc946c1acbbdbf9144
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-30 11:31:13 +0000

    Make testing to the embedded map

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -1,19 +1,33 @@
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
-
-var ready;
-ready = function() {
-
-  $( function() {
-     $( "#datepicker" ).datepicker();
-   } );
-
+$(document).ready(function(){
   var date = new Date();
   var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
   $('#datepicker').datepicker({
-      minDate: today
+    minDate: today
   });
 
-};
+  $('#location').click(function(){
+    alert("entre")
+  });
+
+});
+
+//
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
+//
+// var ready;
+// ready = function() {
+//
+//   $( function() {
+//      $( "#datepicker" ).datepicker();
+//    } );
+//
+//   var date = new Date();
+//   var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+//
+//   $('#datepicker').datepicker({
+//       minDate: today
+//   });
+//
+// };
