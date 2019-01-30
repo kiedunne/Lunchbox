f64bbabce9208c9ae69946b765b8cbddda17f14b
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-30 12:24:42 +0000

    Switch map with spots

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -17,3 +17,15 @@ ready = function() {
   });
 
 };
+
+function DisplayMap() {
+  var x = document.getElementById("allspots");
+  var y = document.getElementById("mapsrender");
+  if (x.style.display === "none") {
+  y.style.display = "none";
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+  y.style.display = "block";
+}
+}
